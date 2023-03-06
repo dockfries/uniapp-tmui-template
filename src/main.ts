55f240b4b91adb76cd55176822a9a9c91b793fa3
dockfries <yucarl77@foxmail.com>
2023-03-06 16:27:16 +0800

    feat: pinia数据持久化

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createSSRApp } from "vue";
 import * as Pinia from "pinia";
+import { createPersistedState } from "pinia-plugin-persistedstate";
 import tmui from "./tmui";
 import App from "./App.vue";
 
 export function createApp() {
   const app = createSSRApp(App);
-  app.use(Pinia.createPinia());
+  const pinia = Pinia.createPinia();
+  const compatUniPinia = createPersistedState({
+    storage: { getItem: uni.getStorageSync, setItem: uni.setStorageSync },
+  });
+  pinia.use(compatUniPinia);
+  app.use(pinia);
   app.use(tmui);
-  return {
-    app,
-    Pinia,
-  };
+  return { app, Pinia };
 }
--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     tmuiCss() as PluginOption,
     Components(),
     AutoImport({
-      include: [/\.vue$/, /\.nvue$/],
+      include: [/\.n?vue$/],
       imports: ["vue", "pinia", "uni-app"],
       eslintrc: { enabled: true },
     }),
