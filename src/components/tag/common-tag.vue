21792862c1be50a3ec7275d18fe7d79d3fe579ee
dockfries <yucarl77@foxmail.com>
2023-08-16 16:12:36 +0800

    feat: tag和icon-title组件

---
+++ src/components/tag/common-tag.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+interface CommonTagProps {
+  size?: string;
+  type?: string;
+  label?: string;
+}
+const props = withDefaults(defineProps<CommonTagProps>(), { type: "warning" });
+
+const tagStyle = computed(() => {
+  let colorByType = "";
+  let size = "";
+
+  switch (props.type) {
+    case "success":
+      colorByType = `_text-_wn_01A63E _bg-_wn_E8F6ED _border-_wn_BAE5C8`;
+      break;
+    case "warning":
+      colorByType = `_text-warning-500 _bg-warning-50 _border-warning-200`;
+      break;
+    case "primary":
+      colorByType = `_text-primary-500 _bg-primary-50 _border-primary-200`;
+      break;
+    default:
+      break;
+  }
+  switch (props.size) {
+    case "sm":
+      size = `_px-8rpx _h-32rpx _font-normal _text-20rpx _rounded-4rpx`;
+      break;
+    default:
+      size = "_px-16rpx _h-48rpx _font-medium _text-24rpx _rounded-10rpx";
+      break;
+  }
+
+  return ["f-center", "_border-2rpx _border-solid", colorByType, size];
+});
+</script>
+
+<template>
+  <view :class="tagStyle">{{ props.label }}</view>
+</template>
---
+++ src/components/title/icon-title.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface IconTitleProps {
+  _class?: string;
+  icon: string;
+  title: string;
+}
+
+defineProps<IconTitleProps>();
+</script>
+
+<template>
+  <view class="_p-24rpx _flex _gap-16rpx _items-center" :class="[_class || '']">
+    <view class="_text-48rpx" :class="[icon]"></view>
+    <view class="_font-bold _text-36rpx">{{ title }}</view>
+  </view>
+</template>
